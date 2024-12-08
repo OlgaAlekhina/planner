8c66b32ca1988a656e0670d2c459a3e06f503b1f
olga <olga-alekhina@rambler.ru>
2024-12-09 02:27:59 +0300

    add userprofile model to users app

--- planner/planner/urls.py
+++ planner/planner/urls.py
@@ -19,11 +19,14 @@ from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Episyche Technologies",
+        title="Planner API",
         default_version='v1',),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -31,5 +34,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(pattern_name='schema-swagger-ui', permanent=True)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- planner/users/models.py
+++ planner/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+USER_SEX = [
+	('M', 'Male'),
+	('F', 'Female')
+]
+
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	avatar = models.ImageField(upload_to="avatars", blank=True)
+	sex = models.CharField(max_length=1, choices=USER_SEX, blank=True)
+	birthday = models.DateField(blank=True, null=True)
+	ya_login = models.CharField(max_length=50, blank=True)
+
+	def __str__(self):
+		return self.user.username
+
+
