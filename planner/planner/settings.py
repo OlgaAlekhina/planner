c6f8ca3ce1f9468a5a7a0dceba5047546f57d923
Olga Alekhina <alekhina.olga@gmail.com>
2024-12-18 18:43:42 +0300

    change static url

--- planner/planner/settings.py
+++ planner/planner/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'planner/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'planner/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
