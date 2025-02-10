3034d607b0a29d2e6eb4660f0a73443078c61298
Ольга Алехина <alekhina.olga@gmail.com>
2025-02-10 11:11:08 +0300

    add signup code to admin console

--- planner/users/admin.py
+++ planner/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile, Group, GroupUser
+from .models import UserProfile, Group, GroupUser, SignupCode
 
 admin.site.register(UserProfile)
 admin.site.register(Group)
 admin.site.register(GroupUser)
+admin.site.register(SignupCode)
