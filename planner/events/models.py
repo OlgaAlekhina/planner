fa1a0feea632799e1eebb26d18f33bbb3ba76747
OlgaAlekhina <olga-alekhina@rambler.ru>
2025-02-21 01:09:03 +0300

    allow null value in location field of event

--- planner/events/models.py
+++ planner/events/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Event(models.Model):
 	title = models.CharField(max_length=100)
-	location = models.CharField(max_length=100, blank=True)
+	location = models.CharField(max_length=100, blank=True, null=True)
 	start_date = models.DateField()
 	end_date = models.DateField()
 	start_time = models.TimeField(blank=True, null=True)
