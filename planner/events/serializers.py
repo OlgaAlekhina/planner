9661af099b85b97808b8bcb5e08b1e36bbc3a321
Ольга Алехина <alekhina.olga@gmail.com>
2025-03-10 02:35:01 +0300

    customize serialization of byweekday eventmeta field

--- planner/events/serializers.py
+++ planner/events/serializers.py
@@ -10,9 +10,23 @@ class EventSerializer(serializers.ModelSerializer):
 		fields = ('id', 'title', 'location', 'start_date', 'end_date', 'start_time', 'end_time', 'users')
 
 
+# class CustomByweekdayField(serializers.CharField):
+# 	""" Кастомное поле для перевода значения byweekday из строки в кортеж """
+# 	def to_representation(self, instance):
+# 		return tuple(map(int, instance.split(',')))
+
+
 class EventMetaSerializer(serializers.ModelSerializer):
 	""" Сериализатор для метаданных события """
+	# byweekday = CustomByweekdayField(read_only=True)
 
 	class Meta:
 		model = EventMeta
-		exclude = ('id', 'event')
+		fields = ('freq', 'interval', 'byweekday', 'bymonthday')
+
+	def to_representation(self, instance):
+		ret = super().to_representation(instance)
+		# переводим строку, содержащую дни недели, в кортеж чисел
+		if ret['byweekday']:
+			ret['byweekday'] = tuple(map(int, ret['byweekday'].split(',')))
+		return ret
--- planner/events/services.py
+++ planner/events/services.py
@@ -8,12 +8,18 @@ def get_dates(metadata, start_date, end_date, event_start, event_end, end_repeat
 	print("saved_args is", saved_args)
 	start_date = parse(start_date)
 	end_date = parse(end_date)
+	print('Weekly: ', WEEKLY)
 	interval = metadata['interval']
+	# вычисляем дату начала для периодических событий
 	if interval > 1 and event_end < datetime.date(start_date):
+		# если повторение по дням
 		if metadata['freq'] == 3:
 			days_delta = (datetime.date(start_date) - event_start).days
 			if days_delta % interval > 0:
 				start_date += timedelta(days=interval-days_delta % interval)
+		# если повторение по неделям
+		elif metadata['freq'] == 2:
+			pass
 	elif event_start > datetime.date(start_date) or event_start < datetime.date(start_date) <= event_end:
 		start_date = event_start
 	until = end_date if not end_repeat or datetime.date(end_date) <= end_repeat else end_repeat
