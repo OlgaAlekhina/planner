103400b05bb878387acd6db4b80ad81f645033a7
Ольга Алехина <alekhina.olga@gmail.com>
2025-03-14 01:02:49 +0300

    fix all periodic occurrences

--- planner/events/services.py
+++ planner/events/services.py
@@ -16,39 +16,39 @@ def get_dates(metadata, filter_start, filter_end, event_start, event_end, end_re
 		# если событие повторяется по дням
 		if metadata['freq'] == 3:
 			# вычисляем количество дней между стартовой датой поиска (filter_start) и датой начала события (event_start)
-			days_delta = (datetime.date(filter_start) - event_start).days
+			days_count = (datetime.date(filter_start) - event_start).days
 			# если не попали в интервал повторений, определяем новую дату начала повторений
-			if days_delta % interval > 0:
-				filter_start += timedelta(days=interval-days_delta % interval)
+			if days_count % interval > 0:
+				filter_start += timedelta(days=interval - days_count % interval)
 		# если событие повторяется по неделям
 		elif metadata['freq'] == 2:
 			# определяем день недели даты начала события (event_start)
 			event_weekday = event_start.weekday()
 			# определяем день недели стартовой даты поиска (filter_start)
 			startdate_weekday = filter_start.weekday()
-			# определяем дату такого же дня недели, что и event_weekday, но на неделе, где идет поиск событий
+			# определяем дату такого же дня недели, что и event_weekday, но на той неделе, где идет поиск событий
 			nearest_date = datetime.date(filter_start) - timedelta(days=startdate_weekday - event_weekday)
 			# определяем количество дней между датами и проверяем, попали ли мы в правильную неделю
-			if (nearest_date - event_start).days % (7 * interval) != 0:
+			week_count = (nearest_date - event_start).days / 7
+			if week_count % interval != 0:
 				# если мы попали в правильную неделю, то оставляем filter_start, если нет - вычисляем нужную неделю и в качестве старта берем понедельник
-				filter_start = (event_start + timedelta(weeks=interval)) - timedelta(days=event_weekday)
+				filter_start += timedelta(weeks=interval - week_count % interval) - timedelta(days=startdate_weekday)
 		# если событие повторяется по месяцам
 		elif metadata['freq'] == 1:
 			# считаем количество месяцев между стартовой датой поиска (filter_start) и датой начала события (event_start)
 			month_count = (filter_start.year - event_start.year) * 12 + filter_start.month - event_start.month
 			# если мы попали в правильный месяц, то оставляем filter_start, если нет - вычисляем нужный месяц и в качестве старта берем 1-ое число
 			if month_count % interval != 0:
-				filter_start = (event_start + relativedelta(months=interval)).replace(day=1)
+				filter_start += relativedelta(months=interval - month_count % interval)
+				filter_start = filter_start.replace(day=1)
 		# если событие повторяется по годам
 		else:
 			# считаем количество лет между стартовой датой поиска (filter_start) и датой начала события (event_start)
 			year_count = filter_start.year - event_start.year
 			# если мы попали в правильный год, то оставляем filter_start, если нет - вычисляем нужный год и в качестве старта берем 1 января
 			if year_count % interval != 0:
-				print(year_count % interval)
-				filter_start_year = event_start.year + interval - year_count % interval
-				print(filter_start_year)
-				filter_start = datetime(day=1, month=1, year=filter_start_year)
+				filter_start += relativedelta(years=interval - year_count % interval)
+				filter_start = filter_start.replace(day=1, month=1)
 	# если событие начинается позже, чем начало поиска, или начало поиска совпадает или раньше даты окончания события, то стартом будет дата начала события
 	elif event_start > datetime.date(filter_start) or event_start < datetime.date(filter_start) <= event_end:
 		filter_start = event_start
