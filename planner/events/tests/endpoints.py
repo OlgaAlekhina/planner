f63a3be92d1f2cb68514d92d691357b9ca118682
Ольга Алехина <alekhina.olga@gmail.com>
2025-04-07 01:38:22 +0300

    add test for event creating

--- planner/events/tests/endpoints.py
+++ planner/events/tests/endpoints.py
@@ -5,6 +5,25 @@ from .config import config
 api_url = config.API_URL
 test_user_token = config.TEST_USER_TOKEN
 
+event_id = None
+
+def test_create_event():
+    global event_id
+    payload = {"event_data": {
+        "title": "New test",
+        "location": "Moscow",
+        "start_date": "2025-04-03",
+        "end_date": "2025-04-03",
+        "start_time": "14:00:00",
+        "end_time": "16:00:00"
+    }}
+    r = requests.post(f'{api_url}/events/', headers={"Authorization": f"Bearer {test_user_token}"}, json=payload)
+    event_id = r.json().get('data').get('event_data').get('id')
+    print('event_id: ', event_id)
+    assert r.status_code == 201
+
+
 def test_get_event():
-    r = requests.get(f'{api_url}/events/1/', headers={"Authorization": f"Bearer {test_user_token}"})
+    global event_id
+    r = requests.get(f'{api_url}/events/{event_id}/', headers={"Authorization": f"Bearer {test_user_token}"})
     assert r.status_code == 200
