c46936a7746c9821b4b82a96f045328193b0b537
Ольга Алехина <alekhina.olga@gmail.com>
2025-04-12 19:32:18 +0300

    add test function for users delete method

--- planner/events/tests/config.py
+++ planner/events/tests/config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 class Config:
 	API_URL = os.getenv('API_URL')
-	TEST_USER_TOKEN = os.getenv('TEST_USER_TOKEN')
 	YANDEX_TOKEN = os.getenv('YANDEX_TOKEN')
 
 config = Config()
--- planner/events/tests/endpoints.py
+++ planner/events/tests/endpoints.py
@@ -3,24 +3,27 @@ from .config import config
 from datetime import date, timedelta
 
 api_url = config.API_URL
-test_user_token = config.TEST_USER_TOKEN
 yandex_token = config.YANDEX_TOKEN
 
 event_id = None
-test_token = None
+test_user_token = None
+test_user_id = None
 
 def test_yandex_auth():
-    global test_token
+    global test_user_token
+    global test_user_id
     payload = {
       "oauth_token": yandex_token
     }
     r = requests.post(f'{api_url}/users/yandex_auth/', json=payload)
-    test_token = r.json().get('data').get('user_auth_token')
+    test_user_token = r.json().get('data').get('user_auth_token')
+    test_user_id = r.json().get('data').get('user_data').get('id')
     assert r.status_code == 200
 
 
 def test_create_event():
     global event_id
+    global test_user_token
     today = str(date.today())
     payload = {"event_data": {
         "title": "New test",
@@ -37,12 +40,14 @@ def test_create_event():
 
 def test_get_event():
     global event_id
+    global test_user_token
     r = requests.get(f'{api_url}/events/{event_id}/', headers={"Authorization": f"Bearer {test_user_token}"})
     assert r.status_code == 200
 
 
 def test_patch_event():
     global event_id
+    global test_user_token
     tomorrow = str(date.today() + timedelta(days=1))
     payload = {"event_data": {
         "title": "Updated test",
@@ -55,5 +60,13 @@ def test_patch_event():
 
 def test_delete_event():
     global event_id
+    global test_user_token
     r = requests.delete(f'{api_url}/events/{event_id}/', headers={"Authorization": f"Bearer {test_user_token}"})
+    assert r.status_code == 204
+
+
+def test_delete_user():
+    global test_user_id
+    global test_user_token
+    r = requests.delete(f'{api_url}/users/{test_user_id}/', headers={"Authorization": f"Bearer {test_user_token}"})
     assert r.status_code == 204
