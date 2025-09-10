040dfbb985a5989d5745bf35398be33beff43fa6
OlgaAlekhina <olga-alekhina@rambler.ru>
2025-09-10 21:09:27 +0300

    add serializer for tasks

--- planner/notes/serializers.py
+++ planner/notes/serializers.py
@@ -1,7 +1,5 @@
-# serializers.py
 from rest_framework import serializers
-from .models import Note
-from django.contrib.auth.models import User
+from .models import Note, Task
 
 
 class NoteSerializer(serializers.ModelSerializer):
@@ -12,6 +10,12 @@ class NoteSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'text', 'create_at', 'update_at']
         read_only_fields = ['id', 'create_at', 'update_at']
 
-    # def validate_title(self, value):
-    #     """Allow empty title since it will be auto-generated"""
-    #     return value or ''
+
+class TaskSerializer(serializers.ModelSerializer):
+    """ Сериализатор для задач """
+
+    class Meta:
+        model = Task
+        fields = ['id', 'text', 'date', 'time', 'important', 'done', 'create_at', 'update_at']
+        read_only_fields = ['id', 'create_at', 'update_at']
+
