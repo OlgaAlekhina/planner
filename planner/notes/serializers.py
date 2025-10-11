670d4b777bbd186c967e05dca9208d550e3f14ac
OlgaAlekhina <olga-alekhina@rambler.ru>
2025-10-11 16:48:54 +0300

    add serializers for list and listitem

--- planner/notes/serializers.py
+++ planner/notes/serializers.py
@@ -1,5 +1,6 @@
+from django.db import transaction
 from rest_framework import serializers
-from .models import Note, Task
+from .models import Note, Task, ListItem, List
 
 
 class NoteSerializer(serializers.ModelSerializer):
@@ -18,4 +19,42 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ['id', 'text', 'date', 'time', 'important', 'done', 'create_at', 'update_at']
         read_only_fields = ['id', 'create_at', 'update_at']
+
+
+class ListItemSerializer(serializers.ModelSerializer):
+    """ Сериализатор для элементов списка """
+    class Meta:
+        model = ListItem
+        fields = ['id', 'text', 'create_at', 'checked']
+        read_only_fields = ['id', 'create_at']
+
+
+class ListSerializer(serializers.ModelSerializer):
+    """ Сериализатор для списков """
+    items = ListItemSerializer(many=True)
+
+    class Meta:
+        model = List
+        fields = ['id', 'title', 'create_at', 'update_at', 'items']
+        read_only_fields = ['id', 'create_at', 'update_at']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items', [])
+        if not validated_data.get('title') and items_data:
+            first_text = items_data[0]['text']
+            validated_data['title'] = first_text[:47] + '...' if len(first_text) > 50 else first_text
+
+        with transaction.atomic():
+            list_obj = List.objects.create(**validated_data)
+
+            if items_data:
+                list_items = [
+                    ListItem(list=list_obj, **item_data)
+                    for item_data in items_data
+                ]
+                ListItem.objects.bulk_create(list_items)
+
+        return list_obj
+
+
 
