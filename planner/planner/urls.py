3e1470ede9fec453d54a53520c67197a9c8a6b40
OlgaAlekhina <olga-alekhina@rambler.ru>
2026-05-09 01:24:21 +0300

    add redirect url

--- planner/planner/urls.py
+++ planner/planner/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -54,6 +55,7 @@ router.register(r'recipe_categories', RecipeCategoryViewSet, basename='recipe_ca
 urlpatterns = [
     path('planner/admin/', admin.site.urls),
     path('planner/', main_page, name='home'),
+    path('planner/invite', RedirectView.as_view(pattern_name='home', permanent=True)),
     path('planner/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('planner/api-auth/', include('rest_framework.urls')),
     path('planner/api/', include(router.urls)),
