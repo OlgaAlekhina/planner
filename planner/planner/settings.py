57b4cc6df343747b7f1c3e2ab9cd9852f9b711c6
OlgaAlekhina <olga-alekhina@rambler.ru>
2026-06-01 22:59:38 +0300

    add pipeline for prod

--- planner/planner/settings.py
+++ planner/planner/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 # Для перехода на https (только для ПРОДА!!!)
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 # Application definition
 
